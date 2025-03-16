b3342676e0c0807ca0abef09357c6aa439f11427
ahtsham05 <ahtshamyounas0321@gmail.com>
2025-03-15 19:57:44 -0700

    AI Part Done!

--- src/app/(app)/u/[username]/page.tsx
+++ src/app/(app)/u/[username]/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Textarea } from "@/components/ui/textarea"
 import { useParams } from 'next/navigation'
 import { Button } from '@/components/ui/button'
@@ -14,11 +14,12 @@ import axios from 'axios'
 const page = () => {
   const params = useParams()
   const username = params.username
+  const [messages, setMessages] = useState<Array<string>>([])
 
   type CreatemessageValidationSchema = z.infer<typeof messageValidationSchema>
-  
+
   const form = useForm<CreatemessageValidationSchema>({
-    resolver:zodResolver(messageValidationSchema),
+    resolver: zodResolver(messageValidationSchema),
     defaultValues: {
       message: "",
       createdAt: new Date(),
@@ -26,19 +27,19 @@ const page = () => {
   })
 
 
-  const { register, handleSubmit , formState : {errors , isSubmitted} } = form
+  const { register, handleSubmit, setValue, formState: { errors, isSubmitted } } = form
 
-  const SendMessage = useCallback(async (data : CreatemessageValidationSchema)=>{
+  const SendMessage = useCallback(async (data: CreatemessageValidationSchema) => {
     try {
-      const response = await axios.post(`/api/send-message`,{...data,username})
-      const { data : responseData } = response
-      if(responseData.success){
+      const response = await axios.post(`/api/send-message`, { ...data, username })
+      const { data: responseData } = response
+      if (responseData.success) {
         toast({
           title: "Success",
           description: "Message sent successfully",
         })
         form.reset()
-      }else {
+      } else {
         toast({
           title: "Error",
           description: responseData.message,
@@ -51,17 +52,40 @@ const page = () => {
         description: "Failed to send message",
       })
     }
-  },[])
+  }, [])
 
-  const suggestNewMessages = useCallback(async()=>{
+  const suggestNewMessages = async () => {
     try {
       const response = await axios.post(`/api/suggest-messages`)
-      const { data : responseData } = response
-      console.log("suggestNewMessages",responseData)
+      const { data: responseData } = response
+
+      if (!responseData) {
+        console.error("No response data received");
+        return;
+      }
+
+      // Find the first and last occurrence of double quotes
+      const parts = responseData.data.content.split('"');
+      console.log("Extracted Parts:", parts);
+      // If the content contains double quotes, extract questions from the second part.
+      let messagesSuggested = [];
+
+      if (parts.length > 1) {
+        messagesSuggested = parts[1].split("||"); // Extract questions from the second part
+        setMessages(messagesSuggested);
+      }
+
     } catch (error) {
       console.error("error suggesting new messages", error)
     }
-  },[])
+  }
+
+  // useEffect(()=>{
+  //   suggestNewMessages()
+  //   return () => {
+  //     // cleanup
+  //   }
+  // },[])
 
   return (
     <div className='p-4 container mx-auto'>
@@ -71,7 +95,7 @@ const page = () => {
         </h1>
         <div className='max-w-xl w-full grid gap-2'>
           <p className='font-semibold text-neutral-800'>Send Anonymous message to @{username}</p>
-          <Textarea placeholder='Write Anonymous message here!' {...register("message")}/>
+          <Textarea placeholder='Write Anonymous message here!' {...register("message")} />
           {
             isSubmitted && errors.message && (
               <p className='text-red-500'>{errors.message.message}</p>
@@ -79,17 +103,28 @@ const page = () => {
           }
           <Button onClick={handleSubmit(SendMessage)}>Send message</Button>
         </div>
-        {/* <div className='w-full max-w-xl grid gap-2'>
+        <div className='w-full max-w-xl grid gap-2'>
           <Button className='w-fit' onClick={suggestNewMessages}>Suggest New Messages</Button>
-          <h1 className='font-semibold text-neutral-800'>Click on any message below to select it.</h1>
-          <div className='border rounded p-2 lg:p-4 grid gap-2'>
-            <h1 className='font-semibold text-xl text-neutral-800'>Messages</h1>
-            <div className='border p-2 rounded'>Message 1</div>
-            <div className='border p-2 rounded'>Message 1</div>
-            <div className='border p-2 rounded'>Message 1</div>
-            <div className='border p-2 rounded'>Message 1</div>
-          </div>
-        </div> */}
+          {
+            messages.length === 0 ? (
+              <h1 className='font-semibold text-neutral-800'>Click on given button For get AI messages</h1>
+            ) : (
+              <h1 className='font-semibold text-neutral-800'>Click on any message below to select it.</h1>
+            )
+          }
+          {
+            messages[0] && (
+              <div className='border rounded p-2 lg:p-4 grid gap-2'>
+                <h1 className='font-semibold text-xl text-neutral-800'>Messages</h1>
+                {messages?.map((message, index) => (
+                  <div key={index} className='border p-2 rounded' onClick={() => {
+                    setValue('message', message)
+                  }}>{message}</div>
+                ))}
+              </div>
+            )
+          }
+        </div>
       </div>
     </div>
   )
--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,28 +1,35 @@
-import OpenAI from 'openai';
-import { NextResponse } from 'next/server';
+// import OpenAI from 'openai';
+import Groq from 'groq-sdk';
 
-const openai = new OpenAI({
-  baseURL: 'https://api.deepseek.com',
-  apiKey: process.env.DEEPSEEK_API_KEY,
+const client = new Groq({
+  apiKey: process.env.groq_key, // This is the default and can be omitted
 });
 
+// const openai = new OpenAI({
+  //   baseURL: 'https://api.openai.com/v1',  // Default is 'https://api.openai.com/v1'
+  //   apiKey: '',  // Your OpenAI API key
+// });
 
 export async function POST(req: Request) {
   try {
     const prompt =
-      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
-
-      const completion = await openai.chat.completions.create({
-          model: "deepseek-chat",
-          messages: [
-              {
-                  role: "user",
-                  content: prompt,
-              },
-          ],
-      });
+    "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
     
-    console.log(completion.choices[0].message);
+      // const completion = await openai.chat.completions.create({
+      //     model: "gpt-3.5-turbo",
+      //     messages: [
+      //         {
+      //             role: "user",
+      //             content: prompt,
+      //         },
+      //     ],
+      // });
+     
+      const completion = await client.chat.completions.create({
+        messages: [{ role: 'user', content: prompt }],
+        model: 'llama3-8b-8192',
+      });
+      // console.log("Chat completion =>",completion)
 
     // Return the streamed response manually
     return  Response.json({
@@ -32,21 +39,6 @@ export async function POST(req: Request) {
       error: false,
     });
   } catch (error) {
-    if (error instanceof OpenAI.APIError) {
-      const { name, status, headers, message } = error;
-      return NextResponse.json(
-        {
-          name,
-          status,
-          message 
-        }, 
-        { status });
-    } else {
-      console.error('An unexpected error occurred:', error);
-      return NextResponse.json(
-        { error: 'An unexpected error occurred' },
-        { status: 500 }
-      );
-    }
+   console.log("Chat completion error",error)
   }
 }
